74d286f9680341bc0e2cd349228934bc6da3aea6
Tomoatsu Shimada <shimada@walbrix.com>
2016-01-23 18:46:47 +0900

    WIP

--- build.sbt
+++ build.sbt
@@ -4,3 +4,4 @@ enablePlugins(JettyPlugin)
 
 libraryDependencies += "javax.servlet" % "javax.servlet-api" % "3.0.1" % "provided"
 libraryDependencies += "org.scalatra" % "scalatra_2.11" % "2.4.0"
+libraryDependencies += "org.mnode.ical4j" % "ical4j" % "1.0.7"
--- src/main/scala/com/walbrix/groupsession/ICalServlet.scala
+++ src/main/scala/com/walbrix/groupsession/ICalServlet.scala
@@ -1,5 +1,29 @@
 package com.walbrix.groupsession
 
+// http://build.mnode.org/projects/ical4j/apidocs/
+import net.fortuna.ical4j.model.Calendar
+import net.fortuna.ical4j.model.component.VEvent
+
 class ICalServlet extends org.scalatra.ScalatraServlet {
-  
+  val apiRoot = Option(System.getenv("GROUPSESSION_API_ROOT")).getOrElse("http://localhost:8080/api")
+
+  get("/") {
+    "/LOGINID.ics?password=PASSWORD"
+  }
+
+  def getICal(loginId:String, password:String):Any = {
+    println(loginId)
+    println(password)
+    "Hello, World!"
+  }
+
+  get("/:loginId.txt") {
+    contentType = "text/plain"
+    getICal(params("loginId"), params.get("password").getOrElse(halt(403, "403 Forbidden: password not provided")))
+  }
+
+  get("/:loginId.ics") {
+    contentType = "text/calendar"
+    getICal(params("loginId"), params.get("password").getOrElse(halt(403, "403 Forbidden: password not provided")))
+  }
 }
